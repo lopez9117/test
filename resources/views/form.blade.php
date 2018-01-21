22632e2fc3d0373537969ecbf9e1b4891b2349ba
lopez9117 <lu.lopez@misena.edu.co>
2018-01-21 17:28:03 -0500

    Agregando el requiered en el front

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,14 +1,14 @@
 <div class="form-group">
 	{!! Form::label('address', 'Direccion del sitio') !!}
-	{!! Form::text('address', null, ['class' => 'form-control']) !!}
+	{!! Form::text('address', null, ['class' => 'form-control','required' => 'required']) !!}
 </div>
 <div class="form-group">
 	{!! Form::label('latitude', 'Latitud del sitio') !!}
-	{!! Form::text('latitude', null, ['class' => 'form-control']) !!}
+	{!! Form::text('latitude', null, ['class' => 'form-control','required' => 'required']) !!}
 </div>
 <div class="form-group">
 	{!! Form::label('longitude', 'Longitud del sitio') !!}
-	{!! Form::text('longitude', null, ['class' => 'form-control']) !!}
+	{!! Form::text('longitude', null, ['class' => 'form-control','required' => 'required']) !!}
 </div>
 <div class="form-group">
 	{!! Form::submit('ENVIAR', ['class' => 'btn btn-primary']) !!}
